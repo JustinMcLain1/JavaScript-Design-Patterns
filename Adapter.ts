23db4b38fd6608fe94792d04b527bbc102eb62cb
JustinMcLain1 <justinkeithmclain@gmail.com>
2022-08-31 16:03:07 -0500

    Update Adapter.ts

--- Adapter.ts
+++ Adapter.ts
@@ -6,6 +6,86 @@ the adapter implements the interface of one object and wraps the other one.
 It can be implemented in all popular programming languages.
 */
 
+//--------------------------------------
 
 // Class adapter
+
+/* This implementation uses inheritance: the adapter inherits interfaces from 
+both objects at the same time. Note that this approach can only be implemented 
+in programming languages that support multiple inheritance, such as C++.
+*/
+
+
+/* The Class Adapter doesn’t need to wrap any objects because it inherits behaviors from both the client and the service. 
+The adaptation happens within the overridden methods. The resulting adapter can be used in place of an existing client class.
+
+
+This example of the Adapter pattern is based on the classic conflict between square pegs and round hole
+
+
+
+#PSUEDOCODE
+
+// Say you have two classes with compatible interfaces:
+// RoundHole and RoundPeg.
+class RoundHole is
+    constructor RoundHole(radius) { ... }
+
+    method getRadius() is
+        // Return the radius of the hole.
+
+    method fits(peg: RoundPeg) is
+        return this.getRadius() >= peg.getRadius()
+
+class RoundPeg is
+    constructor RoundPeg(radius) { ... }
+
+    method getRadius() is
+        // Return the radius of the peg.
+
+
+// But there's an incompatible class: SquarePeg.
+class SquarePeg is
+    constructor SquarePeg(width) { ... }
+
+    method getWidth() is
+        // Return the square peg width.
+
+
+// An adapter class lets you fit square pegs into round holes.
+// It extends the RoundPeg class to let the adapter objects act
+// as round pegs.
+class SquarePegAdapter extends RoundPeg is
+    // In reality, the adapter contains an instance of the
+    // SquarePeg class.
+    private field peg: SquarePeg
+
+    constructor SquarePegAdapter(peg: SquarePeg) is
+        this.peg = peg
+
+    method getRadius() is
+        // The adapter pretends that it's a round peg with a
+        // radius that could fit the square peg that the adapter
+        // actually wraps.
+        return peg.getWidth() * Math.sqrt(2) / 2
+
+
+// Somewhere in client code.
+hole = new RoundHole(5)
+rpeg = new RoundPeg(5)
+hole.fits(rpeg) // true
+
+small_sqpeg = new SquarePeg(5)
+large_sqpeg = new SquarePeg(10)
+hole.fits(small_sqpeg) // this won't compile (incompatible types)
+
+small_sqpeg_adapter = new SquarePegAdapter(small_sqpeg)
+large_sqpeg_adapter = new SquarePegAdapter(large_sqpeg)
+hole.fits(small_sqpeg_adapter) // true
+hole.fits(large_sqpeg_adapter) // false
+
+*/
+
+
+//--------------------------------------
 
