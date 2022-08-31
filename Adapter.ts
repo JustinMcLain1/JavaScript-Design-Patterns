45e031cf7eb18a3a2cc7dfdd2f9c703f3e3bfa77
JustinMcLain1 <justinkeithmclain@gmail.com>
2022-08-31 16:08:14 -0500

    more Info

--- Adapter.ts
+++ Adapter.ts
@@ -89,3 +89,14 @@ hole.fits(large_sqpeg_adapter) // false
 
 //--------------------------------------
 
+//APPLICABILITY
+
+/*
+Use the Adapter class when you want to use some existing class,
+but its interface isn’t compatible with the rest of your code.
+
+The Adapter pattern lets you create a middle-layer class that serves
+ as a translator between your code and a legacy class, a 3rd-party class 
+ or any other class with a weird interface.
+
+ */
