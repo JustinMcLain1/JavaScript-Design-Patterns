c1631ce86c03b281b49fc2dadf126017507fc5cc
JustinMcLain1 <justinkeithmclain@gmail.com>
2022-08-31 16:01:50 -0500

    adapter pattern

---
+++ Adapter.ts
@@ -0,0 +1,11 @@
+// Object Adapater
+
+/* Object adapter
+This implementation uses the object composition principle: 
+the adapter implements the interface of one object and wraps the other one. 
+It can be implemented in all popular programming languages.
+*/
+
+
+// Class adapter
+
